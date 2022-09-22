243e4367f1470efb25e1e5049a6ad3b844c60a03
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-09-22 15:29:49 -0300

    feat: cria as validações de login

--- back-end/src/services/User.js
+++ back-end/src/services/User.js
@@ -4,7 +4,11 @@ const BaseService = require('./Base');
 
 class LoginService extends BaseService {
   async login(login) {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (login.password.length < 6) handleThrowError('Password must be at least 6 characters', 401);
+    if (!emailRegex.test(login.email)) handleThrowError('Invalid email address', 401);
     const user = await this.repository.getOneUser(login.email);
+    if (!user) handleThrowError('User not found', 404);
 
     const isValid = user.password === md5(login.password);
     if (!isValid) handleThrowError('Incorrect email or password', 401);
