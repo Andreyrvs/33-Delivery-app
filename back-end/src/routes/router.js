1123eccb7965eb653a486e6e35a784188982a168
gabsgregorio <gabsufrrj@gmail.com>
2022-09-23 15:34:00 -0300

    feat: adiciona a rota /customer/products ao express

--- back-end/src/routes/router.js
+++ back-end/src/routes/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
 
 const route = express.Router();
 
 route.use(userRouter);
+route.use(productRouter);
 
 module.exports = route;
