f77a83a6435bebed880724885617b073d3fabd0b
dedojs <xandresousax@gmail.com>
2022-09-23 15:51:39 -0300

    main-group-20 req1 ao 10

--- front-end/src/components/Register.jsx
+++ front-end/src/components/Register.jsx
@@ -16,8 +16,15 @@ export default function Register() {
     name: nameUser,
     email: emailUser,
     password: passwordUser,
+  };
+
+  const userLocal = {
+    name: nameUser,
+    email: emailUser,
+    password: passwordUser,
     role: 'customer',
   };
+
   const CREATED = 201;
   const CONFLICT = 409;
 
@@ -33,10 +40,11 @@ export default function Register() {
 
   const addUser = async (event) => {
     event.preventDefault();
-    console.log(newUser);
+    // console.log(newUser);
     const result = await fetchPost(url, newUser);
-
+    console.log(result);
     if (result.status === CREATED) {
+      localStorage.setItem('data', JSON.stringify(userLocal));
       history.push('/customer/products');
     } if (result.status === CONFLICT) {
       setMsgError('Usuário já existe');
