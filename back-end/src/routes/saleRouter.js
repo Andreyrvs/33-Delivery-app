70f0a11ae590daba1ed03c4af21fa6e11528d2e0
gabsgregorio <gabsufrrj@gmail.com>
2022-09-26 16:27:57 -0300

    feat: cria a operação get da rota de checkout

--- back-end/src/routes/saleRouter.js
+++ back-end/src/routes/saleRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const Factory = require('../Factory');
+const SaleService = require('../services/Sale');
 
 const SaleController = Factory.sale().saleController;
 
 const saleRouter = express.Router();
 
+saleRouter.get('/customer/checkout', SaleController.read);
 saleRouter.post('/customer/checkout', SaleController.create);
 
 module.exports = saleRouter;
