ff92b37674dba1f516953bccf9975332887db2e1
gabsgregorio <gabsufrrj@gmail.com>
2022-09-26 16:32:56 -0300

    fix: corrige nome de rota

--- back-end/src/routes/saleRouter.js
+++ back-end/src/routes/saleRouter.js
@@ -6,7 +6,7 @@ const SaleController = Factory.sale().saleController;
 
 const saleRouter = express.Router();
 
-saleRouter.get('/customer/checkout', SaleController.read);
+saleRouter.get('/customer/orders', SaleController.read);
 saleRouter.post('/customer/checkout', SaleController.create);
 
 module.exports = saleRouter;
