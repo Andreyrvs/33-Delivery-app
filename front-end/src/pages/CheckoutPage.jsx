463b4320a352ceef3ce2ea720053dec3e6a09b7d
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-27 00:55:07 -0300

    fix: (Pages) Client - remove itens não usados

--- front-end/src/pages/CheckoutPage.jsx
+++ front-end/src/pages/CheckoutPage.jsx
@@ -1,5 +1,4 @@
 import { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import FinalizeCard from '../components/FinalizeCard';
 import DeliveryDetails from '../components/DeliveryDetails';
 import Header from '../components/Header';
@@ -8,20 +7,15 @@ import MyContext from '../context/MyContext';
 
 export default function CheckoutPage() {
   const { cart } = useContext(MyContext);
-  // const [newCart, setNewCart] = useState([cart]);
 
   useEffect(() => {
   }, [cart]);
   return (
     <div>
-      <Header pageName="Checkout" />
+      <Header pageName="PRODUTOS" />
       <section className="finalize-card">
         <FinalizeCard />
       </section>
-
-      <Link to="/customer/products">
-        <p>Voltar</p>
-      </Link>
       <DeliveryDetails />
     </div>
   );
