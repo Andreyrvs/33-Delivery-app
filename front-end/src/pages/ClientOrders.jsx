200644e734096b44858319e70df2014b005a8f36
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-27 00:56:32 -0300

    chore: (Pages) Orders - adiciona esqueleto pagina dos pedidos

---
+++ front-end/src/pages/ClientOrders.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function ClientOrders() {
+  return (
+    <div>
+      <Header pageName="PRODUTOS" />
+      <section>
+        ClientOrders
+        ClientOrders
+        ClientOrders
+        ClientOrders
+      </section>
+    </div>
+  );
+}
