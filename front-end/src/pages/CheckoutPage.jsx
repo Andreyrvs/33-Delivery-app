5cea8e46aa5aa3d91ef2ca1edd92bc0407083ba9
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-27 00:26:12 -0300

    Feat: (Pages) - Adiciona pagina de checkout e taz o componente do card

--- front-end/src/pages/CheckoutPage.jsx
+++ front-end/src/pages/CheckoutPage.jsx
@@ -1,34 +1,19 @@
-import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import FinalizeCard from '../components/FinalizeCard';
 import Header from '../components/Header';
-import MyContext from '../context/MyContext';
 import '../css/Checkout.css';
 
 export default function CheckoutPage() {
-  const { cart, totalValue } = useContext(MyContext);
-  // const [newCart, setNewCart] = useState([cart]);
-
-  useEffect(() => {
-  }, [cart]);
   return (
     <div>
       <Header pageName="Checkout" />
-      <div className="checkoutContainer">
-        { cart && (
-          cart.map((item) => (
-            <div key={ item.id } className="itemContainer">
-              <p>{item.id}</p>
-              <p>{item.name}</p>
-              <p>{item.qtd}</p>
-              <p>{item.price}</p>
-            </div>
-          ))
-        ) }
-      </div>
+      <section className="finalize-card">
+        <FinalizeCard />
+      </section>
+
       <Link to="/customer/products">
         <p>Voltar</p>
       </Link>
-      <p>{ totalValue }</p>
     </div>
   );
 }
