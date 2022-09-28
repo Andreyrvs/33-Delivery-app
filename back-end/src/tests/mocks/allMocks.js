cebcbc026f3134501e0857b9dc488f835fb565f0
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-09-28 15:47:29 -0300

    feat: test(adicionando mocks)

--- back-end/src/tests/mocks/allMocks.js
+++ back-end/src/tests/mocks/allMocks.js
@@ -1,27 +1,36 @@
 const mockLoginIncorrect = {
-  name: "Admin",
-  email: "adm@deliveryapp.com",
-  password: "senhaerrada",
+  "name": "Admin",
+  "email": "adm@deliveryapp.com",
+  "password": "senhaerrada",
 };
 
 const mockLoginCorrect = {
-  name: "Admin",
-  email: "adm@deliveryapp.com",
-  password: "--adm2@22!!--",
+  "name": "Admin",
+  "email": "adm@deliveryapp.com",
+  "password": "--adm2@22!!--",
 };
 
 const mockLogin = {
-  name: "Admin",
-  email: "adm@deliveryapp.com",
-  password: "a4c86edecc5aee06eff8fdeda69e0d04",
-  role: "admin",
+  "name": "Admin",
+  "email": "adm@deliveryapp.com",
+  "password": "a4c86edecc5aee06eff8fdeda69e0d04",
+  "role": "admin",
 };
 
 const newUser = {
-  name: 'Novo usuário',
-  email: 'user@deliveryapp.com',
-  password: 'user1234',
+  "name": 'New usuário',
+  "email": 'user@deliveryapp.com',
+  "role": "customer",
+	"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTm9tZSBEYSBQZXNzb2EgVXN1w6FyaWEiLCJlbWFpbCI6ImVtYWlsQGRvbWluaW8uY29tIiwicm9sZSI6ImN1c3RvbWVyIn0.s5cmiyY16yViCXkHuzWekxkMeYBi75eT8uJnSbfadNE"
 };
 
-module.exports = {mockLogin, mockLoginCorrect, mockLoginIncorrect, newUser};
+const newUserWhithId = {
+  id: 1,
+  name: 'New usuário',
+  email: 'user@deliveryapp.com',
+  password: "a4c86edecc5aee06eff8fdeda69e0d04",
+  role: 'customer',
+};
+
+module.exports = {mockLogin, mockLoginCorrect, mockLoginIncorrect, newUser, newUserWhithId};
 
--- back-end/src/tests/unit/service/userService.test.js
+++ back-end/src/tests/unit/service/userService.test.js
@@ -2,24 +2,44 @@ const Factory = require('../../../Factory');
 
 const sinon = require('sinon');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-const { expect } = chai;
-const { mockLogin, mockLoginCorrect, mockLoginIncorrect, newUser } = require('../../mocks/allMocks');
+const app = require('../../../api/app');
+const jwt = require('../../../helpers/generateToken');
+const { mockLogin, mockLoginCorrect, mockLoginIncorrect, newUser, newUserWhithId } = require('../../mocks/allMocks');
 // const { mockArray } = require('../../mocks/allMocks')
 const models = require('../../../database/models');
+const generateToken = require('../../../helpers/generateToken');
+chai.use(chaiHttp);
+const { expect } = chai;
 
 
-describe('Testa a função  da camada models', () => {
+describe('Testa a função  da camada service login', () => {
 
   const userRepo = Factory.user().userService;
 
   afterEach(sinon.restore);
 
-  describe("quando create da certo", () => {
+  describe("quando login da certo", () => {
     it("sucessfully", async () => {
-      sinon.stub(models.User, 'create').resolves();
-      const response = await userRepo.create();
-      expect(response).to.be.deep.equal();
+      sinon.stub(models.User, 'findOne').resolves({
+        id: 1,
+        name: 'New usuário',
+        email: 'user@deliveryapp.com',
+        password: "a4c86edecc5aee06eff8fdeda69e0d04",
+        role: 'customer',
+      })
+      const response = await chai.request(app)
+      .post('/login')
+      .set({ 'Authorization': token ({
+        role: "admin",
+      })
+    })
+    // sinon.stub(models.User, 'getOneUser').resolves(newUserWhithId);
+      // .send({ email: newUser.email, password: '123456'});
+      // expect(response.body).to.be.deep.equal(newUser);
+      expect(response.status).to.have.status(200);
+      expect(chaiHttpResponse.body).to.have.a.property('role');
     });
   });
 });
